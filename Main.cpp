19b8f6810e857aed44ff3fff27734520a6f0a79b
Hifza-Kiran <hifza kiran 549@gmail.com>
2026-05-13 08:14:44 -0700

    Learned how to push code via Visual studio Code and added a main cpp file to my repository

---
+++ Main.cpp
@@ -0,0 +1,35 @@
+// #include <iostream>
+// using namespace std;
+
+// class Complex {
+//     double real, imag;
+// public:
+//     Complex() {
+//         real = 0.0;
+//         imag = 0.0;
+//         cout << "Default constructor called!!" << endl;
+//     }
+//     Complex(double r, double i) {
+//         real = r;
+//         imag = i;
+//         cout << "parameterized  constructor called!!" << endl;
+//     }
+//     //Overload + operator
+//     Complex operator+(const Complex& other) const {
+//         return Complex(real + other.real, imag + other.imag);
+//     }
+//     void display()const {
+//         cout << real << "+" << "imag" << endl;
+// }
+
+// };
+
+// int main() {
+//     Complex a(3, 2);
+//         Complex b(1, 7);
+//         Complex c;
+//         c = a + b;
+//         c.display();
+  
+// }
+
