03dd0bbcb857d72324e0bfb71a1d7332b070e3f1
Hifza-Kiran <hifza kiran 549@gmail.com>
2026-05-14 08:46:18 -0700

    Change my code for assigment

--- source.cpp
+++ source.cpp
@@ -2,19 +2,20 @@
 #include "class.h"
 using namespace std;
 int main(){
-    Date date1; // Default constructor
-    Date date2(15, 8, 2021); // Parameterized constructor
+    Date date1; 
+    Date date2(15, 8, 2021); 
     cout << "Default Date: ";
     date1.display();
     cout << "Parameterized Date: ";
     date2.display();
-    // Modifying date1 using setter methods
+   
+    
     date1.setDay(10);
     date1.setMonth(12);
     date1.setYear(2022);
     cout << "Modified Date: ";
     date1.display();
     return 0;
-    
+
 }
 
