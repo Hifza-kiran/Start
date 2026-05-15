0655a56a3ef0935558a96616add29427ae111c0b
Hifza-Kiran <hifza kiran 549@gmail.com>
2026-05-15 10:31:59 -0700

    Added new function declarations and static member for enhanced Date class features.

--- class.h
+++ class.h
@@ -1,19 +1,34 @@
 #pragma once
 #include<iostream>
-class Date{
-    private:
-int day;
-int month;
-int year;
-public:
-Date();
-Date(int d, int m, int y);
-void setDay(int d);
-void setMonth(int m);
-void setYear(int y);
-int getDay()const;
-int getMonth()const;
-int getYear()const;
-void display();
 
+class Date{
+private:
+    int day;
+    int month;
+    int year;
+
+    static int objectCount;
+
+public:
+    Date();
+    Date(int d, int m, int y);
+
+    void setDay(int d);
+    void setMonth(int m);
+    void setYear(int y);
+
+    int getDay()const;
+    int getMonth()const;
+    int getYear()const;
+
+    void display();
+
+    // Added NEW FUNCTIONS
+    bool isLeapYear();
+    bool isValidDate();
+    void displayLongFormat();
+    void nextDay();
+    bool isEqual(Date d);
+
+    static int getObjectCount();
 };
