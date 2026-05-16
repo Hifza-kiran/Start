2b6928b4c61c68169323ab17ded588bb9fcf7aea
Hifza-Kiran <hifza kiran 549@gmail.com>
2026-05-16 09:20:43 -0700

    Updated class.h with new Date function declarations

--- class.h
+++ class.h
@@ -2,6 +2,7 @@
 #include<iostream>
 
 class Date{
+
 private:
     int day;
     int month;
@@ -10,25 +11,35 @@ private:
     static int objectCount;
 
 public:
+
+    // Constructors
     Date();
     Date(int d, int m, int y);
 
+    // Setters
     void setDay(int d);
     void setMonth(int m);
     void setYear(int y);
 
+    // Getters
     int getDay()const;
     int getMonth()const;
     int getYear()const;
 
+    // Display function
     void display();
 
-    // Added NEW FUNCTIONS
+    // Additional functionalities
     bool isLeapYear();
     bool isValidDate();
     void displayLongFormat();
     void nextDay();
+    void previousDay();
+    int daysInMonth();
     bool isEqual(Date d);
+    void swapDates(Date &d);
+    void copyDate(Date d);
 
+    // Static function
     static int getObjectCount();
 };
