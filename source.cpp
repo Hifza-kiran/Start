2ebee73cc7f91b1adf8d256516b47142d230a515
Hifza-Kiran <hifza kiran 549@gmail.com>
2026-05-17 08:58:43 -0700

    Updated main to test operator overloading features of Date class

--- source.cpp
+++ source.cpp
@@ -1,91 +1,46 @@
-#include<iostream>
+#include <iostream>
 #include "class.h"
 
 using namespace std;
 
-int main(){
+int main() {
 
-    Date date1;
-    Date date2(15, 8, 2021);
-    Date date3(15, 8, 2021);
-    Date date4(1, 1, 2025);
+    Date d1(10, 5, 2024);
+    Date d2(15, 8, 2021);
+    Date d3;
 
-    // Default and parameterized constructors
-    cout << "Default Date: ";
-    date1.display();
+    cout << "Initial Dates:\n";
+    cout << d1 << endl;
+    cout << d2 << endl;
 
-    cout << "Parameterized Date: ";
-    date2.display();
+    // ==
+    if (d2 == d3)
+        cout << "Equal\n";
+    else
+        cout << "Not Equal\n";
 
-    // Modify date
-    date1.setDay(10);
-    date1.setMonth(12);
-    date1.setYear(2022);
+    // <
+    if (d2 < d1)
+        cout << "d2 is earlier\n";
 
-    cout << "Modified Date: ";
-    date1.display();
+    // ++ prefix
+    ++d1;
+    cout << "After ++d1: " << d1 << endl;
 
-    // Leap year check
-    if(date1.isLeapYear()){
-        cout << "Leap Year" << endl;
-    }
-    else{
-        cout << "Not a Leap Year" << endl;
-    }
+    // ++ postfix
+    d1++;
+    cout << "After d1++: " << d1 << endl;
 
-    // Date validation
-    if(date1.isValidDate()){
-        cout << "Valid Date" << endl;
-    }
-    else{
-        cout << "Invalid Date" << endl;
-    }
+    // -- prefix
+    --d1;
+    cout << "After --d1: " << d1 << endl;
 
-    // Long format display
-    cout << "Long Format: ";
-    date1.displayLongFormat();
+    // assignment
+    d3 = d2;
+    cout << "After assignment d3 = d2: " << d3 << endl;
 
-    // Next day
-    cout << "Next Day: ";
-    date1.nextDay();
-    date1.display();
-
-    // Previous day
-    cout << "Previous Day: ";
-    date4.previousDay();
-    date4.display();
-
-    // Days in month
-    cout << "Days in Current Month: ";
-    cout << date4.daysInMonth() << endl;
-
-    // Compare dates
-    if(date2.isEqual(date3)){
-        cout << "Dates are Equal" << endl;
-    }
-    else{
-        cout << "Dates are Not Equal" << endl;
-    }
-
-    // Swap dates
-    cout << "\nBefore Swapping:" << endl;
-    date1.display();
-    date2.display();
-
-    date1.swapDates(date2);
-
-    cout << "After Swapping:" << endl;
-    date1.display();
-    date2.display();
-
-    // Copy date
-    cout << "Copied Date: ";
-    date4.copyDate(date2);
-    date4.display();
-
-    // Object counter
-    cout << "Total Objects Created: ";
-    cout << Date::getObjectCount() << endl;
+    // object count
+    cout << "Objects created: " << Date::getObjectCount() << endl;
 
     return 0;
 }
